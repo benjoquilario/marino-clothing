566f7859551de47905bdeb2c51700428a5f6194f
iambenjo <82529126+benjoquilario@users.noreply.github.com>
2024-10-28 09:33:44 +0000

    feat: delete generatedButton and import helpers for uploading photo

--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -1,12 +1,6 @@
-import {
-  generateUploadButton,
-  generateUploadDropzone,
-  generateReactHelpers,
-} from "@uploadthing/react"
-import type { OurFileRouter } from "../app/api/uplodthing/core"
+import { generateReactHelpers } from "@uploadthing/react"
 
-export const UploadButton = generateUploadButton<OurFileRouter>()
-export const UploadDropzone = generateUploadDropzone<OurFileRouter>()
+import type { OurFileRouter } from "@/app/api/uploadthing/core"
 
 export const { useUploadThing, uploadFiles } =
   generateReactHelpers<OurFileRouter>()
