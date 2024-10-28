0bb150748397de28666cc7b1c38a346915f7c12f
iambenjo <82529126+benjoquilario@users.noreply.github.com>
2024-10-28 09:37:06 +0000

    feat: added formatBytes for images sizes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
-import { CartItem } from "./validators/cart"
+import { CartItem } from "./validations/cart"
 import qs from "query-string"
 
 export function cn(...inputs: ClassValue[]) {
@@ -57,4 +57,20 @@ export function formUrlQuery({
     },
     { skipNull: true }
   )
+}
+
+export function formatBytes(
+  bytes: number,
+  decimals = 0,
+  sizeType: "accurate" | "normal" = "normal"
+) {
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB"]
+  const accurateSizes = ["Bytes", "KiB", "MiB", "GiB", "TiB"]
+  if (bytes === 0) return "0 Byte"
+  const i = Math.floor(Math.log(bytes) / Math.log(1024))
+  return `${(bytes / Math.pow(1024, i)).toFixed(decimals)} ${
+    sizeType === "accurate"
+      ? (accurateSizes[i] ?? "Bytest")
+      : (sizes[i] ?? "Bytes")
+  }`
 }
