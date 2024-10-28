7f8a3d45bc585994d9aa3db6ea3c3ac8b3a9a146
iambenjo <82529126+benjoquilario@users.noreply.github.com>
2024-10-28 09:32:26 +0000

    feat: create product schema for adding new products

--- src/lib/validations/product.ts
+++ src/lib/validations/product.ts
@@ -1,8 +1,8 @@
-import * as z from "zod"
-import { createInsertSchema, createSelectSchema } from "drizzle-zod"
 import { products } from "@/db/schema"
 import { colors } from "@/db/schema/color"
 import { sizes } from "@/db/schema/size"
+import { createInsertSchema, createSelectSchema } from "drizzle-zod"
+import * as z from "zod"
 
 export const selectProductSchema = createInsertSchema(products)
 
@@ -36,5 +36,23 @@ export const insertProductSchema = createSelectSchema(products, {
   createdAt: true,
 })
 
+export const createProductSchema = z.object({
+  stock: z.coerce.number().min(0, "Stock must be at least 0"),
+  description: z.string().min(1, "Description is required"),
+  name: z.string().min(1, "Name is required"),
+  category: z.string().min(1, "Category is required"),
+  price: z.string().regex(/^\d+(\.\d{1,2})?$/, {
+    message: "Must be a valid price",
+  }),
+  isFeatured: z.boolean().default(false),
+  slug: z.string().min(1, "Slug is required"),
+  images: z
+    .custom<File[] | undefined | null>()
+    .optional()
+    .nullable()
+    .default(null),
+})
+
+export type CreateProduct = z.infer<typeof createProductSchema>
 export type InsertProduct = z.infer<typeof insertProductSchema>
 export type UpdateProduct = z.infer<typeof updateProductSchema>
